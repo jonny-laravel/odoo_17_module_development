b54337e530bf9ad132c6a185a2b405de49290717
Jonny H Gultom <jonny.h.gultom@gmail.com>
2025-08-28 15:54:45 +0700

    chapter4-1

--- addons/estate/__manifest__.py
+++ addons/estate/__manifest__.py
@@ -3,6 +3,8 @@
     'application':True,
     'depends':['base',],
     'data':[
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
         'security/ir.model.access.csv',
     ],
 }
