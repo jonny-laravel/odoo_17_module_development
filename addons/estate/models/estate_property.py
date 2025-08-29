f238bc4d0d9582a37b94d7f6112358c15a2739e1
Jonny H Gultom <jonny.h.gultom@gmail.com>
2025-08-29 15:01:48 +0700

    Chapter 5-2

--- addons/estate/models/estate_property.py
+++ addons/estate/models/estate_property.py
@@ -1,4 +1,5 @@
 from odoo import fields,models
+from dateutil.relativedelta import relativedelta
 
 class EstateProperty(models.Model):
     _name = "estate.property"
@@ -6,10 +7,10 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False,default=lambda self: fields.datetime.now()+relativedelta(months=3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly = True,copy =False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -22,4 +23,15 @@ class EstateProperty(models.Model):
             ('east','East'),
             ('west','West'),
         ]
+    )
+    active=fields.Boolean(default=True)
+    state = fields.Selection(
+        selection=[
+            ('new','New'),
+            ('over_received','Offer Received'),
+            ('over_accepted','Offer Accepted'),
+            ('sold','Sold'),
+            ('cancelled','Cancelled'),
+        ],
+        default='new',copy = False,required = True,
     )
